ab34733d4465c1361d2758fb364b790b637a896e
Bonswo <89059388+Bonswo@users.noreply.github.com>
2023-11-03 18:33:36 +1100

    Death messages, collision detection
    Death messages tells player how they lost.
    Screen shake strength now applies
    Now cannot "live" on the line behind and ahead of the start and end zones

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pygame, sys, random, numpy, math
 from pygame.locals import *
 from perlin_noise import PerlinNoise
@@ -9,15 +8,34 @@ BROWN = (180,93,63)
 LIGHT_BROWN = (196, 164, 132)
 BABY_BLUE = (176,205,227)
 BLUE_BLACK = (35,40,60)
+FIRST_LEVEL_COLOUR = (35,40,60)
+FINAL_LEVEL_COLUR = (35,40,60) # this(200, 200, 200)
 LIGHT_RED = (208,59,59)
+WHITE = (220, 220, 220)
+
+def interp_colour(n, level):
+    r0, g0, b0 = FIRST_LEVEL_COLOUR
+    r1, g1, b1 = FINAL_LEVEL_COLUR
+    n = n-1
+    return (r0 + level * (r1-r0)/n, 
+            g0 + level * (g1-g0)/n, 
+            b0 + level * (b1-b0)/n)
 
 class Game:
     display_size = (1000, 1000)
     initial_time_limit = 2000
+    overshoot_protection_time = 200
+    level_colours = [FIRST_LEVEL_COLOUR, 
+                     interp_colour(5, 2), 
+                     interp_colour(5, 3), 
+                     interp_colour(5, 4), 
+                     FINAL_LEVEL_COLUR]
+
     def __init__(self):
         pygame.mixer.init()
         pygame.init()
         pygame.font.init()
+        pygame.mouse.set_visible(False)
         self.window = pygame.display.set_mode((1000,1000))
         self.display = pygame.Surface(Game.display_size)
         self.score_font = pygame.font.Font('Oswald.ttf', 600)
@@ -28,8 +46,8 @@ class Game:
         self.current_time_limit = Game.initial_time_limit
         self.timer = Game.initial_time_limit
         self.particle_manager = ParticleManager()
-        self.prev_mpos = (0, 0)
-        
+        self.prev_mpos = pygame.mouse.get_pos()
+        self.overshoot_protection = 0
 
         self.running = True
         self.game_state = 0
@@ -37,11 +55,12 @@ class Game:
         self.score = 0
         self.screen_shake = 0
         self.screen_shake_strength = 5
+        self.death_type = 0
+        self.death_message = None
+        self.level = 0
 
-        self.line_segments = []
         self.bounding_box = None
         self.set_new_bounding_box = False
-        self.previous_mpos = None
 
         pygame.mixer.music.load('MDK - Press Start.mp3')
         pygame.mixer.music.play()
@@ -67,12 +86,8 @@ class Game:
         self.particle_manager.spawn_death_particles(pygame.mouse.get_pos(), 50)
         self.screen_shake = 350
         self.screen_shake_strength = 15
+        self.game_state = 2
         pygame.mixer.Sound.play(self.sound_loss)
-        if self.score > self.high_score:
-            print(f"NEW HIGH SCORE: {self.score}")
-        else:
-            print(f"SCORE: {self.score}")
-            print(f"HIGH SCORE: {self.high_score}")
     
     def on_score(self):
         self.score += 1
@@ -80,14 +95,18 @@ class Game:
         self.particle_manager.bulk_spawn(pygame.mouse.get_pos(), 20)
         self.screen_shake = 150
         self.screen_shake_strength = 5
-        pygame.mixer.Sound.play(random.choice([self.sound_hit2]))
+        self.overshoot_protection = Game.overshoot_protection_time
         if self.score % 10 == 0:
             self.current_time_limit = max(self.current_time_limit - 250, 250)
-            pass
+            self.level += 1 if self.level < 4 else 0
         self.timer = self.current_time_limit
+        pygame.mixer.Sound.play(random.choice([self.sound_hit2]))
+        
+        
 
     def main(self):
-        self.dt = self.clock.tick(60) # milliseconds
+        self.dt = self.clock.tick(144) # milliseconds
+        mpos = pygame.mouse.get_pos()
 
         events = pygame.event.get()
         for event in events:
@@ -96,18 +115,21 @@ class Game:
             
         # Pre-game 
         if self.game_state == 0:
-            self.prev_mpos = pygame.mouse.get_pos()
+            # self.particle_manager.spawn_mouse_trail(self.prev_mpos)
+            
             for event in events:
                 if event.type == MOUSEBUTTONDOWN and event.button == 1:
-                    #start game
-                    print('Game start')
                     self.game_state = 1
                     self.score = 0
+                    self.bounding_box = None
                     self.set_new_bounding_box = True
                     self.timer = Game.initial_time_limit
                     self.current_time_limit = Game.initial_time_limit
+                    self.screen_shake = 150
+                    self.screen_shake_strength = 10
+                    self.death_message = None
+                    self.level = 0
                     pygame.mixer.Sound.play(self.sound_start)
-                    pygame.mouse.set_visible(False)
             self.render_menu()
         
         # Game
@@ -115,50 +137,39 @@ class Game:
             for event in events:
                 if event.type == MOUSEBUTTONUP and event.button == 1:
                     self.on_loss()
-                    print('let go of buton sadge')
-                    self.game_state = 2
+                    self.death_type = 0
             
             if self.timer <= 0:
                 self.on_loss()
-                print('Ran out of time :(')
-                self.game_state = 2
+                self.death_type = 2
         
             if self.set_new_bounding_box:
                 self.set_new_bounding_box = False
-                self.bounding_box = BoundingBox()
+                start_pos = self.bounding_box.end if self.bounding_box else pygame.mouse.get_pos()
+                self.bounding_box = BoundingBox(start_pos)
 
-            
-
-            mpos = pygame.mouse.get_pos()
-            dist_to_line = self.bounding_box.min_distance_to_point(mpos)
-            x0, y0 = self.bounding_box.start
-            x1, y1 = mpos
-            dist_to_start = math.sqrt((y1-y0)**2+(x1-x0)**2)
-            self.particle_manager.spawn_mouse_trail(self.prev_mpos)
-            if dist_to_line > 0 and dist_to_start > 20: # point outside box sadge
+            if not self.bounding_box.point_inside_box(mpos) and self.overshoot_protection == 0: # point outside box sadge
                 self.on_loss()
-                print('mouse outside box sadge')
-                self.game_state = 2
+                self.death_type = 1
         
             dist_to_end = self.bounding_box.point_distance_to_end(mpos)
             if dist_to_end <=0:
                 self.on_score()
 
-            self.prev_mpos = mpos
             self.render_game()
         
             
         # Post-game
         if self.game_state == 2:
-            self.prev_mpos = pygame.mouse.get_pos()
             for event in events:
                 if event.type == MOUSEBUTTONDOWN and event.button == 1:
-                    print('main menu')
                     self.game_state = 0
             self.render_score()
 
-        self.timer -= max(self.dt, 0)
-        self.screen_shake -= max(self.dt, 0)
+        self.prev_mpos = mpos
+        self.timer = max(self.timer - self.dt, 0)
+        self.screen_shake = max(self.screen_shake - self.dt, 0)
+        self.overshoot_protection = max(self.overshoot_protection - self.dt, 0)
         return
     
     @staticmethod
@@ -170,12 +181,16 @@ class Game:
             sub_render(self, *args, **kwargs)
             
             self.particle_manager.render_particles(self.display, self.dt)
+            level_colour = Game.level_colours[self.level]
+            self.particle_manager.spawn_mouse_trail(self.prev_mpos, level_colour)
             pygame.draw.circle(self.display,
-                           BLUE_BLACK,
+                           level_colour,
                            pygame.mouse.get_pos(),
                            10)
             if self.screen_shake > 0:
-                self.window.blit(self.display, (random.randint(-5, 5), random.randint(-5, 5)))
+                displacement_x = random.randint(-self.screen_shake_strength, self.screen_shake_strength)
+                displacement_y = random.randint(-self.screen_shake_strength, self.screen_shake_strength)
+                self.window.blit(self.display, (displacement_x, displacement_y))
             else:
                 self.window.blit(self.display, (0, 0))
             pygame.display.flip()
@@ -192,14 +207,7 @@ class Game:
 
         
         
-        pygame.draw.circle(self.display,
-                           ORANGE,
-                           self.bounding_box.start,
-                           self.bounding_box.width/2)
-        bounding_box_points = get_polyline_points(self.bounding_box.start, self.bounding_box.end, self.bounding_box.width)
-        pygame.draw.polygon(self.display, 
-                            ORANGE,
-                            bounding_box_points)
+        self.bounding_box.render(self.display)
         #dist from end
         x0, y0 = self.bounding_box.start
         x1, y1 = self.bounding_box.end
@@ -207,10 +215,10 @@ class Game:
         percent_progressed = self.timer / self.current_time_limit
         x2 = percent_progressed * (x1-x0) + x0
         y2 = percent_progressed * (y1-y0) + y0
-        bounding_box_points = get_polyline_points((x2,y2), self.bounding_box.end, self.bounding_box.width)
+        progress_box_points = get_polyline_points((x2,y2), self.bounding_box.end, self.bounding_box.width)
         pygame.draw.polygon(self.display, 
                             LIGHT_RED,
-                            bounding_box_points)
+                            progress_box_points)
         
         
         pygame.draw.circle(self.display,
@@ -261,6 +269,19 @@ class Game:
         w, h = info_text.get_size()
         self.display.blit(info_text, (Game.display_size[0]/2 - w/2, 
                                        6* Game.display_size[1]/7 - h/2))
+        
+        death_messages = {
+            0 : ["Don't let go!", "You let go...", 'Hold M1!'],
+            2 : ['Faster!', "Too slow...", "Slowpoke!", "Not fast enough..."],
+            1 : ['Inside the lines!', 'Where are you going?!', 'Maybe slow down :)', 'Bad aim gitgud']
+        }
+
+        if not self.death_message:
+            self.death_message = random.choice(death_messages[self.death_type])
+        death_text = pygame.Font.render(self.info_font, self.death_message, True, LIGHT_BROWN)
+        w, h = death_text.get_size()
+        self.display.blit(death_text, (Game.display_size[0]/2 - w/2, 
+                                       1* Game.display_size[1]/7 - h/2))
         # hs_text = pygame.Font.render(self.title_font, f'{self.high_score}', True, LIGHT_BROWN)
         # w, h = hs_text.get_size()
         # self.display.blit(hs_text, (Game.display_size[0]/2 - w/2, 
@@ -268,11 +289,16 @@ class Game:
         pass
 
 class BoundingBox():
-    def __init__(self, width=100) -> None:
+    def __init__(self, start, width=100) -> None:
         self.width = width
-        self.start = pygame.mouse.get_pos()
+        self.start = start
         self.end = (random.randint(width, Game.display_size[0]-width), 
                     random.randint(width, Game.display_size[1]-width))
+        self.points = get_polyline_points(self.start, self.end, self.width)
+        self.min_x = min([x for (x,y) in self.points])
+        self.min_y = min([y for (x,y) in self.points])
+        self.max_x = max([x for (x,y) in self.points])
+        self.max_y = max([y for (x,y) in self.points])
     
     def min_distance_to_point(self, point):
         x1, y1 = self.start
@@ -282,6 +308,28 @@ class BoundingBox():
         denominator = numpy.sqrt((x2-x1)**2+(y2-y1)**2)
         distance = nominator/denominator - self.width/2
         return distance
+
+    def point_inside_box(self, point):
+        x0, y0 = point
+        inside_line = False
+        if x0 > self.min_x and x0 < self.max_x and y0 > self.min_y and y0 < self.max_y:
+            inside_line = self.min_distance_to_point(point) <= 0
+        
+        inside_start = math.sqrt((self.start[1]-y0)**2+(self.start[0]-x0)**2) < self.width/2
+        inside_end = math.sqrt((self.end[1]-y0)**2+(self.end[0]-x0)**2) < self.width/2
+
+        return inside_start or inside_end or inside_line
+        
+
+    def render(self, display):
+        pygame.draw.circle(display,
+                           ORANGE,
+                           self.start,
+                           self.width/2)
+        
+        pygame.draw.polygon(display, 
+                            ORANGE,
+                            self.points)
 
     def point_distance_to_end(self, point):
         x0, y0 = point
@@ -336,18 +384,19 @@ class DeathParticle():
 
 class MouseTrailParticle():
     initial_duration = 100
-    def __init__(self, start, end):
+    def __init__(self, start, end, colour):
         self.duration = MouseTrailParticle.initial_duration
         self.start = start
         self.end = end
         self.destroy = False
         self.alpha = 255
         self.width = 20
+        self.colour = colour
 
     def render(self, surface, dt):
-        polygon_points = get_polyline_points(self.start, self.end, self.width)
-        # pygame.draw.polygon(surface, BLUE_BLACK, polygon_points)
-        pygame.draw.line(surface, BLUE_BLACK, self.start, self.end, round(self.width))
+        # points=get_polyline_points(self.start, self.end, self.width)
+        # pygame.draw.polygon(surface, self.colour, points, round(self.width)) # works badly because the faces of each polygon don't match up
+        pygame.draw.line(surface, self.colour, self.start, self.end, round(self.width))
         self.duration -= dt
         self.width -=  dt * 20/MouseTrailParticle.initial_duration
         if self.duration <= 0 or self.alpha <= 0:
@@ -381,15 +430,15 @@ class ParticleManager():
         for _ in range(count):
             self.particles.append(DeathParticle(position))
 
-    def spawn_mouse_trail(self, start):
-        self.mouse_trail.append(MouseTrailParticle(start, pygame.mouse.get_pos()))
+    def spawn_mouse_trail(self, start, colour):
+        self.mouse_trail.append(MouseTrailParticle(start, pygame.mouse.get_pos(), colour))
 
 
 def get_polyline_points(start, end, width):
     x0, y0 = start
     x1, y1 = end
     if y1 == y0:
-        y1 += 0.0000001
+        return(start, start, end, end)
 
     rad_angle = math.atan(-(x1-x0)/(y1-y0))
     p1 = (x0 - math.cos(rad_angle) * width/2, y0 - math.sin(rad_angle) * width/2)
@@ -399,6 +448,14 @@ def get_polyline_points(start, end, width):
     p4 = (x1 - math.cos(rad_angle) * width/2, y1 - math.sin(rad_angle) * width/2)
     return (p1, p2, p3, p4)
 
+def draw_polyline(surface, colour, start, end, width):
+    points = get_polyline_points(start, end, width)
+    pygame.draw.polygon(
+        surface,
+        colour,
+        points,
+        width
+    )
 
 game = Game()
 game.run()
